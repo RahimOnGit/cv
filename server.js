e95c89cc7ebe92bb71e23bccf741f1b13ea239af
RahimOnGit <abdalraheem.saif7777@gmail.com>
2026-05-17 11:29:56 +0200

    dynamic cv added

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+app.use(express.json());
+app.use(express.static(__dirname)); // serves dyn-cv.html, cv-data.json etc.
+
+app.post('/save', (req, res) => {
+  const dataPath = path.join(__dirname, 'cv-data.json');
+  fs.writeFileSync(dataPath, JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Dynamic CV server running at http://localhost:${PORT}`));
